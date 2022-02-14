68f738456ed40ba2c47915651d7ad01833d8b106
Adrian Vitor Malze <57869970+Sfiry@users.noreply.github.com>
2022-02-14 14:56:49 -0300

    Update snake.py
    Otimizações

--- Snake/snake.py
+++ Snake/snake.py
@@ -1,11 +1,18 @@
-import pygame, random
+import pygame
+import random
 from pygame.locals import *
+
+
 def on_grid_random():
-    x = random.randint(0, 59)
-    y = random.randint(0, 59)
-    return (x * 10, y * 10)
+    eixo_x = random.randint(0, 59)
+    eixo_y = random.randint(0, 59)
+    return eixo_x * 10, eixo_y * 10
+
+
 def collision(c1, c2):
     return (c1[0] == c2[0]) and (c1[1] == c2[1])
+
+
 UP = 0
 RIGHT = 1
 DOWN = 2
@@ -68,7 +75,7 @@ while not game_over:
         pygame.draw.line(screen, (40, 40, 40), (x, 0), (x, 600))
     for y in range(0, 600, 10):
         pygame.draw.line(screen, (40, 40, 40), (0, y), (600, y))
-    score_font = font.render('Score: %s' % (score), True, (255, 255, 255))
+    score_font = font.render('Score: %s' % score, True, (255, 255, 255))
     score_rect = score_font.get_rect()
     score_rect.topleft = (600 - 120, 10)
     screen.blit(score_font, score_rect)
